f89db59ec9022560d377765ec4a24bef5fa80075
Patrick Steele-Idem <pnidem@gmail.com>
2015-08-27 14:39:42 -0600

    Updated blog

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,3 +1,11 @@
 module.exports = require('marko-widgets').defineComponent({
-    template: require('./template.marko')
+    template: require('./template.marko'),
+    init: function() {
+        this.mobileMenuHidden = true;
+    },
+    handleToggleMenuClick: function() {
+        var nav = document.getElementById('nav');
+        this.mobileMenuHidden = !this.mobileMenuHidden;
+        nav.className = this.mobileMenuHidden ? 'mobile-hidden' : '';
+    }
 });
